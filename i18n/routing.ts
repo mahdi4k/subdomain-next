f5b2808b2f639479aec1b16e2569251c55332dfc
mahdi <mahdifalahati75@gmail.com>
2025-10-18 08:42:51 +0330

    fix routing

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,8 +2,8 @@ import {defineRouting} from 'next-intl/routing';
  
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ['en', 'de'],
+  locales: ['fa','en'],
  
   // Used when no locale matches
-  defaultLocale: 'en'
+  defaultLocale: 'fa'
 });
